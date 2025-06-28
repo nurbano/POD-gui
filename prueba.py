532269ef9c72ee4e705c5614e116c38bc476a3e1
nurbano <urbano.nicolas@gmail.com>
2025-06-27 20:20:43 -0400

    Add configuration file and update RealTimePlot and readserial for enhanced functionality
    - Introduced config.ini for configurable parameters like serial port and test settings.
    - Updated RealTimePlot to read configuration values for timer and serial settings.
    - Modified readserial to accept new parameters for test duration and RPM.
    - Added functionality to stop the test and close the serial port gracefully.
    - Implemented countdown timer and improved data handling in RealTimePlot.
    - Added .gitignore to exclude Excel and CSV files.

--- prueba.py
+++ prueba.py
@@ -5,14 +5,20 @@ from PyQt5 import QtWidgets, QtCore
 import pandas as pd
 import atexit
 import time
-from prueba_serial import readserial, begin_test
+from prueba_serial import readserial
 import threading
+import configparser
+from datetime import datetime
 
 
 class RealTimePlot(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-
+        # Configuración de la aplicación
+        pg.setConfigOption('background', 'b')  # Fondo blanco para el gráfico
+        # Leer y cargar config.ini
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini')
         # Configuración de la ventana
         self.setWindowTitle("Gráfico en Tiempo Real (60 segundos)")
         self.setGeometry(100, 100, 800, 600)
@@ -50,12 +56,13 @@ class RealTimePlot(QtWidgets.QMainWindow):
 
             # Inicializar datos
             buffer_size = 80*60*60  # Número de puntos en el buffer (180 segundos con 80 puntos por segundo)
-            #x = np.linspace(0, 60, buffer_size)  # Eje X: 60 segundos
             x= np.empty(buffer_size)
             x.fill(np.nan)  # Rellenar con NaN para evitar problemas de visualización
+            x[0]= 0  # Establecer el primer valor en 0
+            
             y = np.empty(buffer_size)
             y.fill(np.nan)  # Eje Y: valores iniciales en NaN
-            #y = np.zeros(buffer_size)  # Eje Y: valores iniciales en cero
+            y[0] = 0  # Establecer el primer valor en 0
             current_index = 0  # Índice para rastrear la posición actual en el buffer
             curve = graphWidget.plot(x, y, pen=pg.mkPen(color, width=2,connect='finite'))
             if i == 0:
@@ -76,65 +83,65 @@ class RealTimePlot(QtWidgets.QMainWindow):
         self.countdown_timer = QtCore.QTimer()
         self.countdown_timer.setInterval(1000)
         self.countdown_timer.timeout.connect(self.update_countdown)
-        self.remaining_time = 910 # 60 segundos
+        self.remaining_time= self.config.getint('Ensayo', 't_ensayo', fallback=60)  # Tiempo restante en segundos
         self.countdown_label = QtWidgets.QLabel(f'Tiempo restante: {self.remaining_time:02d} segundos')
         self.countdown_label.setAlignment(QtCore.Qt.AlignCenter)
-        self.remaining_time = 910 # 60 segundos
+        
         self.layout_ensayar.addWidget(self.countdown_label, 4, 0, 1, 2)
-        self.countdown_timer.start()
-        #Abro el puerto serie
-        self.comport = 'COM4'
-        self.baudrate = 115200
+        # Botón para parar el ensayo
+        self.stop_button = QtWidgets.QPushButton("Parar Ensayo")
+        self.stop_button.clicked.connect(self.parar_ensayo)
+        self.layout_ensayar.addWidget(self.stop_button, 4, 2, 1, 1)
+        self.comport = self.config.get('Serial', 'comport', fallback='COM4')  # Puerto serie
+        self.baudrate = self.config.getint('Serial', 'baudrate', fallback=115200)  # Baudrate
+        self.RPM_ensayo = self.config.getint('Ensayo', 'RPM_ensayo', fallback=800)  # RPM del ensayo
+        self.nombre_ensayo = self.config.get('Ensayo', 'nombre_ensayo', fallback='Ensayo')
+        self.nombre_estacion = self.config.get('Ensayo', 'nombre_estacion', fallback='Estación 1')
         self.timestamp = True
         self.tiempo_horas= []
         self.TIMESTAMP= []
         self.VALUES= []
-        self.comienzo = False
+        self.comienzo = []
+        self.comienzo.append(True)  # Usar una lista para poder modificarla dentro del hilo
         self.columns_name= []
         self.index= 0
-        self.ser =  threading.Thread(target=readserial, args=(self.comport, self.baudrate, self.timestamp,self.TIMESTAMP, self.VALUES, self.comienzo, self.columns_name))
+        self.ser =  threading.Thread(target=readserial, args=(self.comport, self.baudrate, self.timestamp,self.TIMESTAMP, self.VALUES, self.comienzo, self.columns_name, self.remaining_time, self.RPM_ensayo))
         self.ser.start()
+        self.flag_countdown = True
         
-
+    def parar_ensayo(self):
+        # Detener el temporizador de actualización del gráfico
+        self.timer.stop()
+        # Detener el temporizador de cuenta regresiva
+        self.countdown_timer.stop()
+        # Enviar comando para detener el ensayo
+        if self.ser.is_alive():
+            self.comienzo[0] = False  # Cambiar el estado de comienzo a False
+            self.ser.join()  # Esperar a que el hilo termine
+            print("Hilo de lectura del puerto serie detenido.")
+            self.close()
+            
     def update_countdown(self):
-        #print("hola")
         self.remaining_time -= 1
-        # minutes = self.remaining_time // 60
-        # seconds = self.remaining_time % 60
-        # self.countdown_label.setText(f"Tiempo restante: {minutes:02d}:{seconds:02d}")
         self.countdown_label.setText(f"Tiempo restante: {self.remaining_time:02d} segundos")
         if self.remaining_time <= 0:
             self.countdown_timer.stop()
-            self.close()
-
 
     def update_plot(self):
-        #start_time = time.time()
+        
         if self.ser.is_alive():
-            #print("Valores: ", len(self.VALUES))
-            #print("Index: ", index)
             if len(self.VALUES) > 1:
+                if self.flag_countdown:
+                    self.flag_countdown = False
+                    self.countdown_timer.start()
                 for actual_index in range(len(self.VALUES)-self.index):
-                    #print(self.VALUES[i])
-                    #print(actual_index, self.VALUES[actual_index+self.index])
-                    #"tiempoMs,tempAmbC,tempObjC,vueltas,celdaCarga"
-                    #try:
                     self.tiempo_horas.append(self.VALUES[actual_index+self.index][0])
                     self.tiempo.append(self.VALUES[actual_index+self.index][1])
                     self.temperatura_amb.append(self.VALUES[actual_index+self.index][2])
                     self.temperatura.append(self.VALUES[actual_index+self.index][3])
                     self.vueltas.append(self.VALUES[actual_index+self.index][4])
                     self.carga.append(self.VALUES[actual_index+self.index][5])
-                    # except IndexError:
-                    #     print(self.VALUES[actual_index+self.index])
-                    #     print("Error: No se pudo acceder al índice  de VALUES. Asegúrate de que los datos estén completos.")
-                    #     self.tiempo_horas.append(0)  # Asignar un valor por defecto si no hay datos de tiempo
-                    #     self.tiempo.append(0)  # Asignar un valor por defecto si no hay datos de tiempo
-                    #     self.temperatura_amb.append(0)  # Asignar un valor por defecto si no hay datos de temperatura ambiente
-                    #     self.temperatura.append(0)  # Asignar un valor por defecto si no hay datos de temperatura
-                    #     self.vueltas.append(0)  # Asignar un valor por defecto si no hay datos de vueltas
-                    #     self.carga.append(0)  # Asignar un valor por defecto si no hay datos de carga
-                    # Guardar datos en disco cada 1000 muestras y limpiar listas para evitar uso excesivo de memoria
+                
                 save_interval = 5000
                 if len(self.vueltas) >= 30:
                         # Diferencia de vueltas en los últimos 3 puntos
@@ -148,7 +155,7 @@ class RealTimePlot(QtWidgets.QMainWindow):
                             velocidad = 0
                         self.velocidad.append(velocidad)
                 else:
-                    self.velocidad.append(0)
+                    self.velocidad.extend([0] * (len(self.VALUES) - len(self.velocidad)))
                 self.index +=actual_index+1
                 if len(self.tiempo) >= save_interval:
                     print(len(self.tiempo_horas), len(self.tiempo), len(self.carga), len(self.temperatura), len(self.temperatura_amb), len(self.vueltas))
@@ -161,8 +168,6 @@ class RealTimePlot(QtWidgets.QMainWindow):
                         "Tempratura Amb. (ºC)": self.temperatura_amb,
                         "Vueltas": self.vueltas
                     }
-                    # for k, v in new_data_dict.items():
-                    #     print(f"{k}: {len(v)}")
                     print("Guardando datos en disco...")
                     # Crear un DataFrame de pandas
                     try:
@@ -201,21 +206,7 @@ class RealTimePlot(QtWidgets.QMainWindow):
                         del self.VALUES[:eliminate_length]
                         print("Valores eliminados para evitar crecimiento indefinido")
                         self.index -= eliminate_length
-                    #print(self.tiempo_horas[-1], self.tiempo[-1], self.temperatura_amb[-1], self.temperatura[-1], self.vueltas[-1], self.carga[-1])
-                    # actual_index es el número de nuevos datos procesados en este ciclo.
-                    # Se usa para saber cuántos elementos nuevos se han añadido y actualizar self.index.
-                    # No necesitas modificarlo, solo asegúrate de que después del for, hagas:
-                #self.index += actual_index
-                    # Calcular velocidad a partir del incremento de las últimas 3 vueltas
-                    
-            #self.TIMESTAMP=[] 
-            #self.VALUES=[]
-                #self.tiempo.append(len(self.tiempo) * 0.1)
                 for i, (graphWidget, curve, x, y, current_index) in enumerate(self.graphs):
-                    # Generar un nuevo valor aleatorio (puedes reemplazar esto con tus datos reales)
-                    #min_val, max_val = self.random_data[self.datos_graficos[i][0]]
-                    #new_value = np.random.uniform(min_val, max_val)
-                    #x= self.tiempo[-actual_index]
                     tiempo_actual=self.tiempo[-actual_index:-1]
                     if i== 0:
                         new_value= self.carga[-actual_index:-1]
@@ -226,7 +217,6 @@ class RealTimePlot(QtWidgets.QMainWindow):
                     elif i== 3:
                         new_value= self.velocidad[-actual_index:-1]
 
-                    
                     # Añadir el nuevo valor al final del buffer
                     for j in range(len(new_value)):
                         if current_index < len(y):
@@ -242,52 +232,54 @@ class RealTimePlot(QtWidgets.QMainWindow):
                             x[-1] = tiempo_actual[j]
 
                         # Actualizar la curva
-                        #print("x: ", x[current_index])
-                        #curve.setData(x, y)
                         curve.setDownsampling(auto=True)
                         curve.setClipToView(True)
                         # Submuestreo para todos los gráficos excepto la carga (i == 0)
-                        #print(f"Actualizando gráfico {i} con {current_index} puntos")
                         if i==0 and current_index > 8:
                             # Submuestrear a 10 veces menos
-                            #print("Submuestreando carga")
                             x_sub = x[:current_index][::8]
                             y_sub = y[:current_index][::8]
-                            #print(f"Actualizando gráfico {i} con {len(x_sub)} puntos")
                             curve.setData(x_sub, y_sub)
                         elif i != 0 and current_index > 80:
                             # Submuestrear a 10 veces menos
-                            #print("Submuestreando temperatura")
                             x_sub = x[:current_index][::80]
                             y_sub = y[:current_index][::80]
                             curve.setData(x_sub, y_sub)
-                            #print(f"Actualizando gráfico {i} con {len(x_sub)} puntos")
                             
                         else:
                             # Para la carga, mostrar todos los datos
-                            #print("Actualizando carga sin submuestreo")
-                            curve.setData(x[:current_index], y[:current_index])
+                            if current_index > 0:
+                                curve.setData(x[:current_index], y[:current_index])
                         self.graphs[i] = (graphWidget, curve, x, y, current_index)
         
-        if self.ser.is_alive == False:
+        else:
+            self.timer.stop()
             print("Murio el hilo")
-            # Aquí puedes agregar cualquier acción adicional que desees realizar al finalizar la lectura del puerto serie
-        
-        #end_time = time.time()
-       # print(end_time - start_time )
-        #print("Tiempo de actualización:", end_time - start_time, "segundos")
+            self.close()
+            
     def closeEvent(self, event):
         # Guardar los datos en un archivo Excel
         # Abrir el archivo binario y guardarlo como xlsx
+        if self.ser.is_alive():
+            self.comienzo[0] = False  # Cambiar el estado de comienzo a False
+            self.ser.join()  # Esperar a que el hilo termine
+            print("Hilo de lectura del puerto serie detenido.")
+        self.save_data()
+        print("Cerrando aplicación...")
+        event.accept()
+
+    def save_data(self):
         try:
+            print("Guardando datos...")
             df = pd.read_pickle("./datos_guardados_temp.pkl")
-            output_path = "./datos_guardados.xlsx"
+            now = datetime.now()
+            fecha_hora = now.strftime("%d-%m-%Y_%H-%M")
+            output_path = f"./{self.nombre_estacion}_{self.nombre_ensayo}_{fecha_hora}.xlsx"
             df.to_excel(output_path, index=False)
             print(f"Datos guardados en {output_path}")
         except Exception as e:
             print(f"Error al guardar datos: {e}")
-        event.accept()
-        # Cerrar la aplicación
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = RealTimePlot()
--- prueba_serial.py
+++ prueba_serial.py
@@ -2,31 +2,24 @@ import serial
 import time
 import pandas as pd
 
-def readserial(comport, baudrate, timestamp=False, TIMESTAMP=[], VALUES=[], comienzo=False, columns_name=[]):
+def readserial(comport, baudrate, timestamp=False, TIMESTAMP=[], VALUES=[], comienzo=[], columns_name=[], t_ensayo=60, RPM_ensayo=800):
 
     ser = serial.Serial(comport, baudrate, timeout=0.1)         # 1/timeout is the frequency at which the port is read
     ser.flushInput()                                            # Flush the input buffer to remove any old data
     ser.flushOutput()                                           # Flush the output buffer to remove any old data
-    #VALUES=[]
-    #ser.write(b'TESTSTART-000800-000015\n')  # Send a string to the serial port
-    while True:
+
+    while comienzo[0]:
 
         data = ser.readline().decode().strip()
-
         if data and timestamp:
             timestamp = time.strftime('%H:%M:%S')
-            
-            #print(f'{timestamp} > {data}')
-        
+          
         if "tiempoMs," in data:
             columns_name = list( map(str, data.split(',')))
             columns_name.insert(0, "timestamp")
             
         elif "," in data:
-            #comienzo = True
-            #print(data)
             try:
-                #print("Convirtiendo datos a float...")
                 values = list(map(float, data.split(',')))
             except ValueError:
                 print(f"Error converting data to float: {data}")
@@ -34,30 +27,20 @@ def readserial(comport, baudrate, timestamp=False, TIMESTAMP=[], VALUES=[], comi
                 continue
             timestamp = time.strftime('%H:%M:%S')
             values.insert(0, timestamp)
-            # Append the values to the list
             VALUES.append(values)
-            
-            
+           
         elif data:
             print(data)
         if data =='Iniciando....':
-            ser.write(bytes('TESTSTART-000800-000900', 'utf-8'))
+            ser.write(bytes(f'TESTSTART-{RPM_ensayo:06d}-{t_ensayo:06d}', 'utf-8'))
+            #ser.write(bytes('TESTSTART-000800-000900', 'utf-8'))
             print('Enviando comando de lectura...')
         if data == 'TESTEND':
             print('Lectura detenida')
-            # df = pd.DataFrame(VALUES, columns=columns_name)
-            # print(df.head())
-            # df.to_csv('data.csv', index=False)
-            # print('Guardando datos en data.csv')
+            ser.close()  # Close the serial port when done
             break
- 
-def begin_test(ser):
-    print('Enviando comando de lectura...')
-    ser.write(bytes('TESTSTART-000800-000005', 'utf-8'))  # Send a string to the serial port
-    
-    #time.sleep(1)  # Wait for 1 second before reading again
-
-if __name__ == '__main__':
-
-    readserial('COM4', 115200, True)  
-   
+    if comienzo[0]== False:
+        print('Enviando comando de parada...')
+        ser.write(bytes('TESTEND', 'utf-8'))  # Send a command to stop the test
+        ser.close()  # Close the serial port when done
+    exit(0)
